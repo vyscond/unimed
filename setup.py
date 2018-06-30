72db9ca78cc40a71c27cf1f111502fbb8f98de4e
Ramon Moraes <vyscond@hades.local>
2018-06-30 18:16:41 -0300

    Fix incorrect dictionary format on setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 cfg = {
-    name: "nest",
-    author: "Ramon Moraes",
-    author_email: "ramon@vyscond.io",
-    version: "0.1.0",
-    description: "Client that return JSON format information from the UNIMED home page",
-    long_description: "".join(open("README.md")),
-    url: "https://github.com/vyscond/unimed",
-    license: "MIT",
-    packages: [
+    "name": "unimed",
+    "author": "Ramon Moraes",
+    "author_email": "ramon@vyscond.io",
+    "version": "0.1.0",
+    "description": "Client that return JSON format information from the UNIMED home page",
+    "long_description": "".join(open("README.md")),
+    "url": "https://github.com/vyscond/unimed",
+    "license": "MIT",
+    "packages": [
         "unimed"
     ],
 }
